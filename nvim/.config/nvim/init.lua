b87413bb60e7383c8029ff61f0d673d627ce9fd9
Aaron Ouellette <aaronjo2@illinois.edu>
2026-01-05 11:07:51 -0600

    some additional settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,6 @@
-vim.g.mapleader = " "
+-- use space as leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- line numbers
 vim.o.number = true
@@ -13,7 +15,7 @@ vim.o.smartindent = true
 -- search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.opt.hlsearch = false
+--vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 -- colorscheme
@@ -27,12 +29,19 @@ vim.cmd.colorscheme('retrobox')
 vim.o.signcolumn = 'yes'
 vim.o.cursorline = true
 vim.o.colorcolumn = "80"
+vim.o.scrolloff = 10
+
+-- new splits go to the right or bottom
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 vim.opt.clipboard = "unnamedplus"
 
 -- <leader>d deletes text without overwriting clipboard
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
+-- clear highlights on search using <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- <leader>pv opens netrw file browser
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
