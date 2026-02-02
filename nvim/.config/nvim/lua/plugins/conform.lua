1901baca477033d9278025093ad4a7249a05fb66
Aaron Ouellette <aaronjo2@illinois.edu>
2026-02-02 16:12:52 -0600

    use ruff for python + only auto-format for specific filetypes

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,16 +3,23 @@ return {
 	opts = {},
 	config = function()
 		require("conform").setup({
-			format_on_save = {
-				timeout_ms = 5000,
-                lsp_format = "fallback",
-			},
+            format_on_save = function(bufnr)
+                -- format on save only for specific  filetypes
+                local fmt_filetypes = { "go", "rust" }
+                if vim.tbl_contains(fmt_filetypes, vim.bo[bufnr].filetype) then
+                    return { timeout_ms = 5000, lsp_format = "fallback" }
+                end
+                return
+            end,
 			formatters_by_ft = {
 				-- c = { "clang-format" },
 				-- cpp = { "clang-format" },
 				-- lua = { "stylua" },
 				go = { "gofmt" },
                 rust = { "rustfmt" },
+                python = { "ruff_organize_imports", "ruff_format" },
+                -- for filetypes that don't have a specific formatter:
+                ["_"] = { "trim_whitespace" }
 			},
 			formatters = {
 				["clang-format"] = {
